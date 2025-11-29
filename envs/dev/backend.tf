d6e100df7796ce17e3bba0f72396a3a28f62bf43
Abu Sayeed <itplusplus0@gmail.com>
2025-11-29 10:09:15 -0500

    updated dev

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,7 @@
+
 terraform {
   backend "s3" {
-    bucket = "group45-dev-tfstate" # change to your bucket
+    bucket = "group45-dev-tfstate" #  change to your bucket
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
